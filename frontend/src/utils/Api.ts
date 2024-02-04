54840d25ecc815217382a88958fa2d416d5128b6
Stas Zaitsev <ststas@yandex.ru>
2024-02-04 17:51:57 +0200

    refactor api code

--- frontend/src/utils/Api.ts
+++ frontend/src/utils/Api.ts
@@ -1,80 +1,41 @@
 import { BASE_URL } from './constants';
 import { Task } from '../types/types';
 
-export function getTasks(): Promise<Task[]> {
+function request(
+  method: string,
+  url: string,
+  data: Task
+): Promise<Task[] | Task | void> {
   const headers: Headers = new Headers();
   headers.set('Content-Type', 'application/json');
   headers.set('Accept', 'application/json');
-  const request: RequestInfo = new Request(`${BASE_URL}/tasks`, {
-    method: 'GET',
-    headers: headers,
-  });
-  return fetch(request)
-    .then((res) => res.json())
-    .then((res) => {
-      return res as Task[];
-    });
-}
-
-export function getTasksById(id: string): Promise<Task> {
-  const headers: Headers = new Headers();
-  headers.set('Content-Type', 'application/json');
-  headers.set('Accept', 'application/json');
-  const request: RequestInfo = new Request(`${BASE_URL}/tasks/${id}`, {
-    method: 'GET',
-    headers: headers,
-  });
-  return fetch(request)
-    .then((res) => res.json())
-    .then((res) => {
-      return res as Task;
-    });
-}
-
-export function createTask(data: Task): Promise<Task> {
-  const headers: Headers = new Headers();
-  headers.set('Content-Type', 'application/json');
-  headers.set('Accept', 'application/json');
-  const request: RequestInfo = new Request(`${BASE_URL}/tasks`, {
-    method: 'POST',
+  const request: RequestInfo = new Request(`${BASE_URL}/tasks/${url}`, {
+    method: method,
     headers: headers,
     body: JSON.stringify(data),
   });
   return fetch(request)
     .then((res) => res.json())
-    .then((newTask) => {
-      return newTask as Task;
-    });
+    .then((res) => res as Task[])
+    .catch((err) => console.error('Error ', err));
 }
 
-export function updateTask(data: Task): Promise<Task> {
-  const headers: Headers = new Headers();
-  headers.set('Content-Type', 'application/json');
-  headers.set('Accept', 'application/json');
-  const taskId = data._id;
-  const request: RequestInfo = new Request(`${BASE_URL}/tasks/${taskId}`, {
-    method: 'PATCH',
-    headers: headers,
-    body: JSON.stringify(data),
-  });
-  return fetch(request)
-    .then((res) => res.json())
-    .then((newTask) => {
-      return newTask as Task;
-    });
+export function getTasks(): Promise<Task[] | Task | void> {
+  return request('GET', '', {} as Task);
 }
 
-export function deleteTask(taskId: string): Promise<Task> {
-  const headers: Headers = new Headers();
-  headers.set('Content-Type', 'application/json');
-  headers.set('Accept', 'application/json');
-  const request: RequestInfo = new Request(`${BASE_URL}/tasks/${taskId}`, {
-    method: 'DELETE',
-    headers: headers,
-  });
-  return fetch(request)
-    .then((res) => res.json())
-    .then((newTask) => {
-      return newTask as Task;
-    });
+export function getTasksById(id: string): Promise<Task[] | Task | void> {
+  return request('GET', id, {} as Task);
+}
+
+export function createTask(data: Task): Promise<Task[] | Task | void> {
+  return request('POST', '', data);
+}
+
+export function updateTask(data: Task): Promise<Task[] | Task | void> {
+  return request('PATCH', data._id, data);
+}
+
+export function deleteTask(taskId: string): Promise<Task[] | Task | void> {
+  return request('PATCH', taskId, {} as Task);
 }
