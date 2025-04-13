06d56eef06df167992aa299e8fb3e041bf62a71f
Stas Zaitsev <ststas@yandex.ru>
2025-04-13 16:36:17 +0300

    fix: change routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,26 @@
 const router = require("express").Router();
 const { handleRouteError } = require("../middlewares/handleRouteError");
+
+router.get("/", (_, res) => {
+  res.status(200).json({ message: "API is working" });
+});
+
+const tasksRouter = require("./tasks");
+
+router.use("/", tasksRouter);
+
+router.all("*", handleRouteError);
+
+module.exports = router;
+
 // const { validateSignUp, validateSignIn } = require("../middlewares/validation");
 // const auth = require("../middlewares/auth");
 // const { login, logout, createUser } = require("../controllers/users");
 
 // const usersRouter = require("./users");
-const tasksRouter = require("./tasks");
 
 // router.post("/signup", validateSignUp(), createUser);
 // router.post("/signin", validateSignIn(), login);
 // router.delete("/signout", logout);
 
 // router.use("/users", auth, usersRouter);
-router.use("/", tasksRouter);
-
-router.all("*", handleRouteError);
-
-module.exports = router;
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -7,10 +7,6 @@ const {
   updateTask,
 } = require("../controllers/tasks");
 
-router.get("/todolist/api", (_, res) => {
-  res.status(200).json({ message: "API is working" });
-});
-
 router.get("/tasks", getTasks);
 router.post("/tasks", createTask);
 router.delete("/tasks/:taskId", deleteTask);
